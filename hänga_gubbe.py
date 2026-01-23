36eda9a61dfa3a7eba0fe8b2bb4352869772e2f0
henrik8772 <henrik.johansson@elev.ga.ntig.se>
2026-01-23 10:02:20 +0100

    hänga_gubbe

--- hänga_gubbe.py
+++ hänga_gubbe.py
@@ -1,27 +1,51 @@
 import random
 
 
-guesses = 8
-play = True
+def hänga_gubbe():
+    guesses = 8
+    play = True
 
-print("Välkommen till Hänga Gubbe!!!")
+    print("Välkommen till Hänga Gubbe!!!")
 
-while play == True:
-    spela = input("Vill du spela? ja/nej: ")
+    while play == True:
+        spela = input("Vill du spela? ja/nej: ")
 
-    if spela == "ja" or "Ja":
-        word_list = open("words.txt", "r")
-        word = random.choice(word_list.readline().split(","))
-        word_guess_längd = len(word)
-        word_guess = ["_"] * word_guess_längd
-        delimiter = ""
-        join_word_guess = delimiter.join(word_guess)
-        print(join_word_guess)
-        guess = input("Gissa på en bokstav!: ")
+        if spela.lower() == "ja":
+            guessed_letters = []
+            word_list = open("words.txt", "r")
+            word = random.choice(word_list.readline().split("," " "))
+            word_length = len(word)
+            word_guess = ["_"] * len(word)
+            delimiter = ""
+            join_word_guess = delimiter.join(word_guess)
+            print("Ordet har", word_length, "bokstäver")
+            print(join_word_guess)
+            while guesses > 0 or "".join(word_guess) != word:
+                guess = input("Gissa på en bokstav!: ")
+                for i, letter in enumerate(word):
+                    if guess == letter:
+                        word_guess[i] = guess
+                        print("".join(word_guess))
+                    # elif guess != letter:
+                        # guessed_letters.append(guess)
+                        # print("guessed letters", guessed_letters)
+            if "".join(word_guess) == word:
+                print("Yippie du vann!!")
+                play_again = input("Vill du spela igen? ja/nej: ")
+                if play_again.lower() == "ja":
+                    hänga_gubbe()
 
-    elif spela == "nej" or "Nej":
-        print("Hejdå! Ses igen")
-        play = False
+                elif play_again.lower() == "nej":
+                    print("Hejdå!!")
+                    break
 
-    else:
-        ValueError
+        elif spela.lower() == "nej":
+            print("Hejdå! Ses igen")
+            play = False
+
+        else:
+            ValueError
+
+
+if __name__ == "__main__":
+    hänga_gubbe()
